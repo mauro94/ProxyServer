e05a8f72698a5a5005e326be775fb759316c1a88
Mauro Amarante <mauro.amarante94@gmail.com>
2015-11-24 19:05:27 -0600

    Threads

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -15,63 +15,77 @@ import java.util.*;
 
 public class ProxyServer {
     public static void main(String args[]) throws IOException {
-
         //variables
-        String inputLine;
-        URL webURL = null;
         ServerSocket proxySocket = null;
         Socket communicationSocket = null;
+        boolean read = true;
 
-        //crear conexion para proxy
-        try {
-            proxySocket = new ServerSocket(5000);
-        }
-        catch (IOException e) {
-            System.out.println(e);
-            System.exit(1);
-        }  
+        try{
+		    proxySocket = new ServerSocket(5000);
 
-        while (true) {
-            try {
-            	//primero el proxy debe de recibir la pagina solicitada
-            	communicationSocket = proxySocket.accept(); 
-        		BufferedReader in = new BufferedReader( new InputStreamReader( communicationSocket.getInputStream()));
-
-        		//leer informacion del navegador
-        		String urlLine = in.readLine();
-
-        		//eliminar datos inecesarios y dejar solo URL
-        		String[] words = urlLine.split(" ");
-        		urlLine = words[1];
-
-	            //crear URL y conexion con URL
-	            webURL = new URL(urlLine);
-	            URLConnection webConection = webURL.openConnection();
-	            webConection.setRequestProperty("User-Agent","");
-				webConection.connect();
-
-	            //leer datos de URL y regresar a navegador datos
-	            BufferedReader inURL = new BufferedReader(new InputStreamReader(webConection.getInputStream()));
-	            PrintStream out = new PrintStream(communicationSocket.getOutputStream());
-	           	
-	            while ((inputLine = inURL.readLine()) != null) { 
-	            	out.println(inputLine);
-				} 
-
-				in.close();
-				inURL.close();
-				out.close();
+            while(read){
+	         	communicationSocket = proxySocket.accept();
+	         	new Threads(communicationSocket).start();
             }
-            catch (UnsupportedEncodingException e) { 
-                System.err.println(e);
-            }
-            catch (MalformedURLException e) {
-                System.err.println(e); 
-            }
-            catch (IOException e) { 
-                System.err.println(e);
-            }
-        }
-    }
+
+		    proxySocket.close();
+		} catch (IOException e){
+			System.err.println(e.getMessage());
+			System.exit(1);
+		}
+	}
 }
-    
+
+
+class Threads extends Thread{
+	//variables
+    private String inputLine;
+    private URL webURL = null;
+    private String urlLine;
+    private String[] words;
+	private BufferedReader inURL;
+    private BufferedReader in;
+    private PrintStream out;
+    private Socket communicationSocket = null;
+    private URLConnection webConection;
+
+    public Threads(Socket communicationSocket){
+        this.communicationSocket = communicationSocket;
+    }
+	public void run(){
+	    try{
+            in = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+            out = new PrintStream(communicationSocket.getOutputStream());
+
+            urlLine = in.readLine();
+
+            words = urlLine.split(" ");
+        	urlLine = words[1];
+
+        	webURL = new URL(urlLine);
+	        webConection = webURL.openConnection();
+	        webConection.connect();
+
+            inURL = new BufferedReader(new InputStreamReader(webConection.getInputStream()));
+
+            while ((inputLine = inURL.readLine()) != null) { 
+            	//System.out.println(inputLine);
+            	out.println(inputLine);
+			} 
+
+            in.close();
+			inURL.close();
+			out.close();
+			communicationSocket.close();
+        }
+        catch (UnsupportedEncodingException e) { 
+            System.err.println(e);
+        }
+        catch (MalformedURLException e) {
+            System.err.println(e); 
+        }
+        catch (IOException e) { 
+            System.err.println(e);
+        }
+	}
+}
